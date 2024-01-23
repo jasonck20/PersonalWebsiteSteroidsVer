872997912400b93a37a166b622c7c8780930189d
jasonck20 <93351028+jasonck20@users.noreply.github.com>
2024-01-22 20:33:28 -0700

    Add files via upload

--- Personalwebsite.js
+++ Personalwebsite.js
@@ -7,7 +7,6 @@ function toggleMenu() {
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
 document.querySelector("h1").onmouseover = event => {
     let iterations = 0;
     
@@ -17,206 +16,42 @@ document.querySelector("h1").onmouseover = event => {
                 if (index < iterations){
                     return event.target.dataset.value[index];
                 }
-                
-                
                 return letters[Math.floor(Math.random()*26)]
             })
             .join("");
         if(iterations >= event.target.dataset.value.length) clearInterval(interval);
         iterations += 1 / 5;
-
-        }, 30);
-    
+    }, 30);
 }
 
-var SCREEN_WIDTH =window.innerWidth;
-	var SCREEN_HEIGHT = window.innerHeight;
 
-	var RADIUS = 50;
+(function () {
 
-	var RADIUS_SCALE = 1;
-	var RADIUS_SCALE_MIN = 1;
-	var RADIUS_SCALE_MAX = 1.5;
+	const link = document.querySelectorAll('nav > .hover-this');
+	const cursor = document.querySelector('.cursor');
 
-	// The number of particles that are used to generate the trail
-	var QUANTITY = 1;
+	const animateit = function (e) {
+		  const span = this.querySelector('span');
+		  const { offsetX: x, offsetY: y } = e,
+		  { offsetWidth: width, offsetHeight: height } = this,
 
-	var canvas;
-	var context;
-	var particles;
+		  move = 25,
+		  xMove = x / width * (move * 2) - move,
+		  yMove = y / height * (move * 2) - move;
 
-	var mouseX = (window.innerWidth - SCREEN_WIDTH) +  Math.floor(Math.random() * 500) + 100  ; //
-	var mouseY = (window.innerHeight) + Math.floor(Math.random() * 500) + 100  ; // - SCREEN_HEIGHT
-	var mouseIsDown = false;
+		  span.style.transform = `translate(${xMove}px, ${yMove}px)`;
 
-	$(document).ready(function(){
-    initTrail()
-	})
+		  if (e.type === 'mouseleave') span.style.transform = '';
+	};
 
-	function initTrail() {
+	const editCursor = e => {
+		  const { clientX: x, clientY: y } = e;
+		  cursor.style.left = x + 'px';
+		  cursor.style.top = y + 'px';
+	};
 
-		canvas = document.getElementById( 'world' );
+	link.forEach(b => b.addEventListener('mousemove', animateit));
+	link.forEach(b => b.addEventListener('mouseleave', animateit));
+	window.addEventListener('mousemove', editCursor);
 
-		if (canvas && canvas.getContext) {
-			context = canvas.getContext('2d');
-
-			// Register event listeners
-			window.addEventListener('load', documentMouseMoveHandler, false);
-			document.addEventListener('mousemove', documentMouseMoveHandler, false);
-			document.addEventListener('mousedown', documentMouseDownHandler, false);
-			document.addEventListener('mouseup', documentMouseUpHandler, false);
-			canvas.addEventListener('touchstart', canvasTouchStartHandler, false);
-			canvas.addEventListener('touchmove', canvasTouchMoveHandler, false);
-			window.addEventListener('resize', windowResizeHandler, false);
-
-
-
-			createParticles();
-
-			windowResizeHandler();
-
-			setInterval( loop, 1000 / 60 );
-
-
-		}
-	}
-
-	function createParticles() {
-
-		console.log(mouseX)
-		console.log(mouseY)
-		particles = [];
-
-		for (var i = 0; i < QUANTITY; i++) {
-			var particle = {
-				position: { x: mouseX, y: mouseY },
-				shift: { x: mouseX, y: mouseY },
-				size: 15,
-				angle: 0,
-				speed: 0.09,
-				// speed: 0.01+Math.random()*0.04,
-				targetSize: 1,
-				fillColor: '#c5c5c5',
-				// fillColor: '#' + (Math.random() * 0x404040 + 0xaaaaaa | 0).toString(16),
-				orbit: RADIUS*.5 + (RADIUS * .5 * Math.random())
-			};
-
-			particles.push( particle );
-		}
-	}
-
-	function documentMouseMoveHandler(event) {
-		mouseX = event.clientX - (window.innerWidth - SCREEN_WIDTH) * .5;
-		//console.log( (window.innerWidth - SCREEN_WIDTH) * .5)
-		mouseY = event.clientY - (window.innerHeight - SCREEN_HEIGHT) * .5;
-		//console.log( (window.innerHeight - SCREEN_HEIGHT) * .5)
-
-		console.log('documentMouseMoveHandler');
-	}
-
-	function documentMouseDownHandler(event) {
-		mouseIsDown = true;
-
-		console.log('documentMouseDownHandler');
-	}
-
-	function documentMouseUpHandler(event) {
-		mouseIsDown = false;
-
-		console.log('documentMouseUpHandler');
-	}
-
-	function canvasTouchStartHandler(event) {
-		if(event.touches.length == 1) {
-			event.preventDefault();
-
-			mouseX = event.touches[0].pageX - (window.innerWidth - SCREEN_WIDTH) * .5;
-			mouseY = event.touches[0].pageY - (window.innerHeight - SCREEN_HEIGHT) * .5;
-		}
-
-		console.log('canvasTouchStartHandler');
-	}
-
-	function canvasTouchMoveHandler(event) {
-		if(event.touches.length == 1) {
-			event.preventDefault();
-
-			mouseX = event.touches[0].pageX - (window.innerWidth - SCREEN_WIDTH) * .5;
-			mouseY = event.touches[0].pageY - (window.innerHeight - SCREEN_HEIGHT) * .5;
-		}
-
-		console.log('canvasTouchMoveHandler');
-	}
-
-	function windowResizeHandler() {
-		//SCREEN_WIDTH = window.innerWidth;
-		//SCREEN_HEIGHT = window.innerHeight;
-
-		canvas.width = SCREEN_WIDTH;
-		canvas.height = SCREEN_HEIGHT;
-
-		canvas.style.position = 'absolute';
-		canvas.style.left = (window.innerWidth - SCREEN_WIDTH) * .5 + 'px';
-		canvas.style.top =  (window.innerHeight - SCREEN_HEIGHT) * .5 + 'px';
-
-		console.log("canvas style", canvas.style.left, canvas.style.top);
-	}
-
-	function loop() {
-
-		if( mouseIsDown ) {
-			// Scale upward to the max scale
-			RADIUS_SCALE += ( RADIUS_SCALE_MAX - RADIUS_SCALE ) * (0.03);
-		}
-		else {
-			// Scale downward to the min scale
-			RADIUS_SCALE -= ( RADIUS_SCALE - RADIUS_SCALE_MIN ) * (0.03);
-		}
-
-		RADIUS_SCALE = Math.min( RADIUS_SCALE, RADIUS_SCALE_MAX );
-
-		// Fade out the lines slowly by drawing a rectangle over the entire canvas
-		// context.fillStyle = 'rgba(0,0,0,0.05)';
-		context.fillStyle = 'rgba(255,255,255,0)';
-   		context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-
-    
-		for (i = 0, len = particles.length; i < len; i++) {
-			var particle = particles[i];
-
-			var lp = { x: particle.position.x, y: particle.position.y };
-
-			// Offset the angle to keep the spin going
-			particle.angle += particle.speed;
-
-			// Follow mouse with some lag
-			particle.shift.x += ( mouseX - particle.shift.x) * (particle.speed);
-			particle.shift.y += ( mouseY - particle.shift.y) * (particle.speed);
-
-			// Apply position
-			particle.position.x = particle.shift.x + Math.cos(i + particle.angle) * (particle.orbit*RADIUS_SCALE);
-			particle.position.y = particle.shift.y + Math.sin(i + particle.angle) * (particle.orbit*RADIUS_SCALE);
-
-			// Limit to screen bounds
-			particle.position.x = Math.max( Math.min( particle.position.x, SCREEN_WIDTH ), 0 );
-			particle.position.y = Math.max( Math.min( particle.position.y, SCREEN_HEIGHT ), 0 );
-
-			particle.size += ( particle.targetSize - particle.size ) * 0.05;
-
-			// If we're at the target size, set a new one. Think of it like a regular day at work.
-			if( Math.round( particle.size ) == Math.round( particle.targetSize ) ) {
-				// particle.targetSize = 1 + Math.random() * 7;
-				particle.targetSize = 2;
-			}
-
-			context.beginPath();
-			context.fillStyle = particle.fillColor;
-			context.strokeStyle = particle.fillColor;
-			context.lineWidth = particle.size;
-			context.moveTo(lp.x, lp.y);
-			context.lineTo(particle.position.x, particle.position.y);
-			context.stroke();
-			context.arc(particle.position.x, particle.position.y, particle.size/2, 0, Math.PI*2, true);
-			context.fill();
-		}
-	}
+})();
