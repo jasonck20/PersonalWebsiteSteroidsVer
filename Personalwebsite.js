47392a45cf079fa2b15b7d307a41751adceeeef2
jasonck20 <93351028+jasonck20@users.noreply.github.com>
2024-01-24 04:06:23 -0700

    Add files via upload

--- Personalwebsite.js
+++ Personalwebsite.js
@@ -55,3 +55,26 @@ document.querySelector("h1").onmouseover = event => {
 	window.addEventListener('mousemove', editCursor);
 
 })();
+
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    document.getElementById('clock').textContent = `${hours}:${minutes}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); 
+
+function updateDateTime() {
+    const now = new Date();
+    // Update Date
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const day = String(now.getDate()).padStart(2, '0');
+    document.getElementById('date').textContent = `${year}-${month}-${day}`;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
